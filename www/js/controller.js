b24cd1dae65e817d0ecb593036d6e334f1957eee
Asha <asha@softstackfactory.org>
2016-07-06 19:56:42 +0000

    working calculator

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,20 +1,45 @@
-angular.module('starter')
+/*global angular*/
+angular.module('starter', [])
     .controller('CalculatorController', function($scope) {
+        $scope.display = 0;
+        var memory = [];
+        var operand = 0;
+
         $scope.clear = function() {
-            // TODO: Clear the display
-            $scope.display = "";
-            //alert('Clear!');
+            $scope.display = 0;
+            operand;
+            memory[0];
         };
 
         $scope.clicked = function(label) {
+            if ($scope.display == '0')
+                $scope.display = [label];
+            else $scope.display.push(label);
+        };
+
+        /*$scope.clicked = function(label) {
             // TODO: Append the label to the display
             $scope.display = label;
             //alert('clicked: ' + label);
-        };
+        };*/
 
         $scope.equals = function() {
-            // TODO: Calculate  & set the display to the result.
-            $scope.display = "";
-            //alert('Equals!');
-        };
-    });
+    var result = eval($scope.display.join(''));
+    $scope.display = [result];
+};
+    });      
+       /* $scope.equals = function() {
+            var equation = $scope.display;
+            var result = calculator.calculate(equation);
+            $scope.display = result;
+        };*/
+
+/*.factory('calculator', function() {
+    var calculate = function(equation) {
+        var parser = new Epsilon.ExpressionParser(equation);
+        return parser.evaluate();
+    };
+ 
+    return {
+        calculator: calculate
+    };*/
